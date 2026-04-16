17fed494b6226124bae431f20cb8e686e8b06982
anonyfriday <duykimvu75@gmail.com>
2026-04-16 22:33:57 +0700

    feat(category): add entity category

---
+++ src/ApiService/NashFridayStore.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace NashFridayStore.Domain.Entities;
+
+internal class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<SubCategory> SubCategories { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
