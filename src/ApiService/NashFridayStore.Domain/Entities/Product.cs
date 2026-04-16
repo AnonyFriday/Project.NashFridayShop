5fb84a8fcfa0f7ea3a0d60487908020078fb3620
anonyfriday <duykimvu75@gmail.com>
2026-04-16 18:39:41 +0700

    feat(product): add entity Product

---
+++ src/ApiService/NashFridayStore.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NashFridayStore.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid SubcategoryId { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal PriceUsd { get; set; }
+    public string ImageUrl { get; set; } = default!;
+    public int Quantity { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime UpdatedAtUtc { get; set; }
+}
