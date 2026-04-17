85c78ed753c9a3929e99ef49692be978b289f678
An0nyFr1day <56812770+AnonyFriday@users.noreply.github.com>
2026-04-17 18:02:29 +0700

    Feature: Add Product entity, configuration, migration

--- src/ApiService/NashFridayStore.API/Program.cs
+++ src/ApiService/NashFridayStore.API/Program.cs
@@ -1,8 +1,11 @@
+using NashFridayStore.Infrastructure.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 WebApplication app = builder.Build();
 
