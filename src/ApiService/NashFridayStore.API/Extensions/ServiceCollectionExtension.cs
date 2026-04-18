2b675ae4d58472ce0448ad457f8d69a6c4f049f6
anonyfriday <duykimvu75@gmail.com>
2026-04-18 14:16:22 +0700

    chore: add fluent validation extensions

--- src/ApiService/NashFridayStore.API/Extensions/ServiceCollectionExtension.cs
+++ src/ApiService/NashFridayStore.API/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
+using System.Reflection;
+using FluentValidation;
 using NashFridayStore.API.ExceptionHandlers;
-using NashFridayStore.API.Exceptions;
 
 namespace NashFridayStore.API.Extensions;
 
@@ -7,9 +8,35 @@ public static class ServiceCollectionExtension
 {
     public static void AddApiServices(this IServiceCollection serviceCollection)
     {
+        // Fluent Validation
+        serviceCollection.AddValidatorsFromAssembly(typeof(ServiceCollectionExtension).Assembly);
+
         // Exception Handlers 
         serviceCollection.AddProblemDetails();
         serviceCollection.AddExceptionHandler<GeneralExceptionHandler>();
         serviceCollection.AddExceptionHandler<InternalServerErrorExceptionHandler>();
+
+        // API Handlers
+        RegisterAllApiHandlers(serviceCollection);
+
+        // Controllers 
+        serviceCollection.AddControllers();
+
+        // Documentation
+        serviceCollection.AddOpenApi();
+    }
+
+    private static void RegisterAllApiHandlers(IServiceCollection serviceCollection)
+    {
+        Assembly assembly = typeof(ServiceCollectionExtension).Assembly;
+
+        IEnumerable<Type> handlers = assembly
+            .GetTypes()
+            .Where(t => t.Name == "Handler" && t.IsClass && !t.IsAbstract && !t.IsInterface);
+
+        foreach (Type handler in handlers)
+        {
+            serviceCollection.AddScoped(handler);
+        }
     }
 }
