b66e9c2fd7b54182275b9cb6e68d3845eaae2d52
anonyfriday <duykimvu75@gmail.com>
2026-04-24 17:40:02 +0700

    feat: add migration for openiddict

--- src/NashFridayStore.IdentityServer/Migrations/20260424103301_add_open_iddict.Designer.cs
+++ src/NashFridayStore.IdentityServer/Migrations/20260424103301_add_open_iddict.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NashFridayStore.IdentityServer.Data;
 
@@ -11,9 +12,11 @@ using NashFridayStore.IdentityServer.Data;
 namespace NashFridayStore.IdentityServer.Migrations
 {
     [DbContext(typeof(IdentityServerDbContext))]
-    partial class IdentityServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424103301_add_open_iddict")]
+    partial class add_open_iddict
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
