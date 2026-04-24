f66d2a2edf444f23c5b4cac63fc81d963ef15d45
anonyfriday <duykimvu75@gmail.com>
2026-04-24 17:27:06 +0700

    feat: add configs for IdentityServer

--- src/NashFridayStore.IdentityServer/Extensions/IdentityServerCollectionExtension.cs
+++ src/NashFridayStore.IdentityServer/Extensions/IdentityServerCollectionExtension.cs
@@ -27,6 +27,9 @@ public static class IdentityServerCollectionExtension
             ConnectionStringsOptions settings = sp.GetRequiredService<IOptions<ConnectionStringsOptions>>().Value;
             options.UseSqlServer(settings.Database);
             options.UseSnakeCaseNamingConvention();
+
+            // Inject Open Iddict tables
+            options.UseOpenIddict();
         });
         services.AddTransient<IdentityServerDbContextSeeder>();
 
@@ -34,5 +37,40 @@ public static class IdentityServerCollectionExtension
         services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
         .AddEntityFrameworkStores<IdentityServerDbContext>()
         .AddDefaultTokenProviders();
+
+        // Add OpenIddict
+        services
+            .AddOpenIddict()
+            .AddCore(opt =>
+            {
+                // storing tokens, scope, authorization, clients
+                opt.UseEntityFrameworkCore().UseDbContext<IdentityServerDbContext>();
+            })
+            .AddServer(opt =>
+            {
+                // enable password flow client credentials
+                opt.AllowClientCredentialsFlow().AllowRefreshTokenFlow(); // Machine to Machine authentication
+                opt.AllowPasswordFlow().AllowRefreshTokenFlow();          // User type username and password
+
+                // Using dev cert, but disable access token  encryption for learning purpose
+                opt
+                    .AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate()
+                    .DisableAccessTokenEncryption();
+
+                // Enable those option, I will authenticate, OpenIddict issue token only
+                opt
+                    .UseAspNetCore() // go through
+                    .EnableAuthorizationEndpointPassthrough()
+                    .EnableTokenEndpointPassthrough()
+                    .EnableEndSessionEndpointPassthrough()
+                    .DisableTransportSecurityRequirement();
+
+                // Setup token, authorize, logout for our own
+                opt.SetAuthorizationEndpointUris("/connect/authorize");
+                opt.SetTokenEndpointUris("/connect/token");
+                opt.SetEndSessionEndpointUris("/connect/logout");
+
+            });
     }
 }
