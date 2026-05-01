fd0f83641f8855b2e7bd442d002040b75b52e0c8
anonyfriday <duykimvu75@gmail.com>
2026-05-01 21:30:55 +0700

    feat(products): add product slice, types for getProducts

--- src/admin-site/app/(admin)/products/page.tsx
+++ src/admin-site/app/(admin)/products/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useGetProductsQuery } from "@/features/products/product.api";
+
 export default function ProductsPage() {
-  return <div>Products</div>;
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error...</div>;
+  }
+
+  return (
+    <div>
+      Products
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  );
 }
