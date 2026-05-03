a605403876615a22739434bf4d0aaa804eff2289
anonyfriday <duykimvu75@gmail.com>
2026-05-03 13:44:28 +0700

    feat: add ArchiveStatusBadge

---
+++ src/admin-site/features/shared/components/ArchiveStatusBadge.tsx
@@ -0,0 +1,38 @@
+interface ArchiveStatusBadgeProps {
+  isDeleted: boolean;
+  className?: string;
+  size?: "xs" | "sm" | "md" | "lg";
+}
+
+export default function ArchiveStatusBadge({ isDeleted, className = "", size = "md" }: ArchiveStatusBadgeProps) {
+  const colorClass = isDeleted ? "text-red-600" : "text-green-600";
+  const sizeClass = size === "md" ? "" : `badge-${size}`;
+
+  return (
+    <div className={`${colorClass} ${sizeClass} font-medium flex items-center gap-1.5 py-4 px-4 ${className}`}>
+      {isDeleted ? (
+        <>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+            <path
+              fillRule="evenodd"
+              d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>Deleted</span>
+        </>
+      ) : (
+        <>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>Active</span>
+        </>
+      )}
+    </div>
+  );
+}
