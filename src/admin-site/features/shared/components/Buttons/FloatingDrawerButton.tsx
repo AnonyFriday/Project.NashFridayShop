1a6ceb63da5ee5d0e3b221fd8b08fa76cbec42d7
anonyfriday <duykimvu75@gmail.com>
2026-05-04 12:28:12 +0700

    refactor: rename component

--- src/admin-site/features/shared/components/Buttons/FloatingDrawerButton.tsx
+++ src/admin-site/features/shared/components/Buttons/FloatingDrawerButton.tsx
@@ -1,10 +1,6 @@
 export default function FloatingDrawerButton() {
   return (
-    <label
-      htmlFor="admin-drawer"
-      className="btn btn-circle btn-primary fixed 
-                 top-3/6 right-4 md:hidden z-50 shadow-lg"
-      aria-label="open sidebar">
+    <label htmlFor="admin-drawer" className="btn btn-circle btn-primary fixed top-3/6 right-4 md:hidden z-50 shadow-lg" aria-label="open sidebar">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
         <path
           strokeLinecap="round"
