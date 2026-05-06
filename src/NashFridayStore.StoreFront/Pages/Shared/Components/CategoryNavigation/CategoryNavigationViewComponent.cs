3d561e9970e97a39f95e615e1d5d7d1397484305
anonyfriday <duykimvu75@gmail.com>
2026-05-07 01:53:46 +0700

    refactor: rename CategoryNavigationVM to CategoryNavigationResponseVM

--- src/NashFridayStore.StoreFront/Pages/Shared/Components/CategoryNavigation/CategoryNavigationViewComponent.cs
+++ src/NashFridayStore.StoreFront/Pages/Shared/Components/CategoryNavigation/CategoryNavigationViewComponent.cs
@@ -5,22 +5,25 @@ namespace NashFridayStore.StoreFront.Pages.Shared.Components.CategoryNavigation;
 
 public class CategoryNavigationViewComponent(ICategoryApiClient categoryApiClient) : ViewComponent
 {
-    public CategoryNavigationVM CategoryNavigationVM { get; set; } = new();
+    public CategoryNavigationResponseVM CategoryNavigationResponseVM { get; set; } = new();
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
         try
         {
             GetAllCategoriesResponse? response = await categoryApiClient.GetAllCategoriesAsync();
-            CategoryNavigationVM.Categories = response?.Items?
-                .Select(c => new CategoryNavigationVM.Category(c.Id.ToString(), c.Name))
-                .ToList() ?? [];
+            CategoryNavigationResponseVM = new CategoryNavigationResponseVM
+            {
+                Categories = response?.Items?
+                    .Select(c => new CategoryNavigationResponseVM.Category(c.Id.ToString(), c.Name))
+                    .ToList() ?? []
+            };
         }
         catch
         {
-            CategoryNavigationVM.Categories = [];
+            CategoryNavigationResponseVM = new CategoryNavigationResponseVM { Categories = [] };
         }
 
-        return View(CategoryNavigationVM);
+        return View(CategoryNavigationResponseVM);
     }
 }
