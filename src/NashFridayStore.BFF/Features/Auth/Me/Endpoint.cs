9cdca68ca8b4fbbdb55f5062cd85e4f0311dfb51
anonyfriday <duykimvu75@gmail.com>
2026-05-08 14:37:55 +0700

    fix: add missing CustomerId into UserInfo endpoint and fix IsAuthenticated

--- src/NashFridayStore.BFF/Features/Auth/Me/Endpoint.cs
+++ src/NashFridayStore.BFF/Features/Auth/Me/Endpoint.cs
@@ -13,15 +13,17 @@ public class Endpoint : ControllerBase
     public IActionResult GetUserInfo()
     {
         // For debugging, you can check exactly what claims are available
-        var allClaims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+#pragma warning disable S125 // Sections of code should not be commented out
+        // var allClaims = User.Claims.Select(c => new { c.Type, c.Value }).ToList();
+#pragma warning restore S125 // Sections of code should not be commented out
 
         return Ok(new
         {
-            IsAuthenticated = true,
+            CustomerId = User.FindFirstValue(OpenIddictConstants.Claims.Subject),
+            IsAuthenticated = User.Identity?.IsAuthenticated ?? false,
             Email = User.FindFirstValue(OpenIddictConstants.Claims.Email),
             PhoneNumber = User.FindFirstValue(OpenIddictConstants.Claims.PhoneNumber),
             Roles = User.FindAll(OpenIddictConstants.Claims.Role).Select(c => c.Value).ToList(),
-            AllClaims = allClaims
         });
     }
 }
