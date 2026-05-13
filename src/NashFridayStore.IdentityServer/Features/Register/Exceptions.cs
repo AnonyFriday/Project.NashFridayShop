19e4e0c1bbd674097ddfe3e07f44c067dea04ae9
anonyfriday <duykimvu75@gmail.com>
2026-05-14 06:56:56 +0700

    feat: add Register function

--- src/NashFridayStore.IdentityServer/Features/Register/Exceptions.cs
+++ src/NashFridayStore.IdentityServer/Features/Register/Exceptions.cs
@@ -1,6 +1,17 @@
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
+using NashFridayStore.IdentityServer.Commons;
+using NashFridayStore.IdentityServer.Commons.Exceptions;
+
 namespace NashFridayStore.IdentityServer.Features.Register;
 
 public static class Exceptions
 {
+    public class ValidationException(IEnumerable<ValidationFailure> errors) : AppValidationException(errors);
 
+    public class IdentityException(IEnumerable<IdentityError> errors) : AppException(
+        AppCts.ProblemDetailsTypes.BadRequest,
+        "Identity error",
+        string.Join(", ", errors.Select(e => e.Description))
+    );
 }
